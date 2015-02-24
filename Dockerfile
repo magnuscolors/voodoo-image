1051e31a46d027683c209fad9dc09dd0458bbe43
Sébastien BEAU <sebastien.beau@akretion.com>
2015-02-24 20:04:32 +0100

    [REF] start refactoring voodoo image, use local file instead of remote file. Use only buildout eggs

--- Dockerfile
+++ Dockerfile
@@ -7,54 +7,29 @@ RUN wget https://s3.amazonaws.com/akretion/packages/wkhtmltox-0.12.1_linux-trust
 
 RUN DEBIAN_FRONTEND=noninteractive && \
     apt-get update && \
-    apt-get install -y python-pip libsasl2-dev python-bzrlib python-pip bzr mercurial libxmlsec1-dev python-libxml2 python-openssl python-pydot python-cups python-dbus && \
+    apt-get install -y libsasl2-dev bzr mercurial libxmlsec1-dev && \
     apt-get clean
-
-RUN mkdir /.devstep/.local && chown developer /.devstep/.local && \
-    touch /.devstep/.viminfo && chown developer /.devstep/.viminfo
 
 RUN sed -i -e"s/^#fsync = on/fsync = off/g" /.devstep/addons/postgresql/conf/postgresql.conf
 
 RUN mkdir -p /workspace && chown developer /workspace
-WORKDIR /workspace
-
-ADD requirements.txt /workspace/requirements.txt
-RUN pip install pip --upgrade
-RUN pip install setuptools --upgrade
-RUN pip install -r requirements.txt
-
-USER developer
-
-RUN /.devstep/bin/configure-addons postgresql
-
-
-RUN wget https://raw.github.com/buildout/buildout/master/bootstrap/bootstrap.py
-RUN wget https://gist.githubusercontent.com/rvalyi/db890269f9c8353a101e/raw/edbfa0b6dcac55e4b5176b0a70ec9102a0b94b9a/buildout.dockerfile.cfg && \
-    python bootstrap.py --allow-site-packages -c buildout.dockerfile.cfg
-
-RUN wget -O- https://gist.githubusercontent.com/rvalyi/0dd63c06310095836062/raw/b1deab1217afc07379fe629c61c976a4c3222837/fake_odoo7 | sh && \
-    cd /workspace && python bin/buildout -c buildout.dockerfile.cfg && \
-    rm -rf /workspace/parts && rm -rf /workspace/etc && rm /workspace/upgrade.py
-
-RUN wget -O- https://gist.githubusercontent.com/rvalyi/9ac3a22cde339aa1ef35/raw/4dac97154c62e31c366f42273a084f143d10fc1e/fake_odoo8 | sh && \
-    cd /workspace && python bin/buildout -c buildout.dockerfile.cfg && \
-    rm -rf /workspace/parts && rm -rf /workspace/etc && rm /workspace/upgrade.py
-
-RUN mkdir /.devstep/addons/voodoo && \
-    mv /workspace/eggs /.devstep/addons/voodoo/eggs && \
-    mv /workspace/develop-eggs /.devstep/addons/voodoo/develop-eggs && \
-    mv /workspace/downloads /.devstep/addons/voodoo/downloads && \
-    rm /workspace/bootstrap.py && \
-    rm /workspace/requirements.txt
-
-USER root
-
-RUN wget https://gist.githubusercontent.com/rvalyi/4e62a50aaef186b85970/raw/6946af101b9b9207a707cf7f445eebedc54809bd/init1 && \
-    mv init1 /.devstep/.profile.d/voodoo.sh && chmod +x /.devstep/.profile.d/voodoo.sh
 
 RUN ln -s /workspace/ak /bin/ak
 
-RUN mkdir -p /.devstep/.ssh && chown -R developer:developer /.devstep/.ssh
 RUN locale-gen pt_BR.UTF-8
 
 USER developer
+
+#Config for developer user
+ADD stack/profile/voodoo.sh /.devstep/.profile.d/voodoo.sh
+RUN mkdir -p /.devstep/.ssh
+RUN mkdir /.devstep/.local && touch /.devstep/.viminfo
+
+#Install postgresql
+RUN /.devstep/bin/configure-addons postgresql
+
+#Pre-build environement for odoo
+ADD stack/build /workspace/
+RUN sh /workspace/build_all
+
+WORKDIR /workspace
---
+++ stack/profile/voodoo.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+force_color_prompt=true
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        # We have color support; assume it's compliant with Ecma-48
+        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+        # a case would tend to support setf rather than setaf.)
+        color_prompt=yes
+    else
+        color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+ESC_SEQ="\x1b[38;5;"
+COL_01=$ESC_SEQ"160;01m"
+COL_02=$ESC_SEQ"196;01m"
+COL_03=$ESC_SEQ"202;01m"
+COL_04=$ESC_SEQ"208;01m"
+COL_05=$ESC_SEQ"214;01m"
+COL_06=$ESC_SEQ"220;01m"
+COL_07=$ESC_SEQ"226;01m"
+COL_08=$ESC_SEQ"190;01m"
+COL_09=$ESC_SEQ"154;01m"
+COL_10=$ESC_SEQ"118;01m"
+COL_11=$ESC_SEQ"046;01m"
+COL_12=$ESC_SEQ"047;01m"
+COL_13=$ESC_SEQ"048;01m"
+COL_14=$ESC_SEQ"049;01m"
+COL_15=$ESC_SEQ"051;01m"
+COL_16=$ESC_SEQ"039;01m"
+COL_17=$ESC_SEQ"027;01m"
+COL_18=$ESC_SEQ"021;01m"
+COL_19=$ESC_SEQ"021;01m"
+COL_20=$ESC_SEQ"057;01m"
+COL_21=$ESC_SEQ"093;01m"
+RESET="\033[m"
+
+printf "$COL_01                                                             _                   \n"
+printf "$COL_03 MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM featuring: MMMMMMMMM| |MMMMMMMMMMMMMMMMMM\n"
+printf "$COL_04 MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM _____  ____   _   _ | |__    ___   _   _ \n"
+printf "$COL_05 MMMMMMMMMMMMMMIIIIIIIIOMMMMMMMMMMMMMM (____ ||  _ \ | | | ||  _ \  / _ \ \ \/ / \n"
+printf "$COL_06 MMMMMMMMMMM\$IIIIIIIIDMMMMMMMMMMMMMMMM / ___ || | | || |_| || |_) )| |_| | >  < \n"
+printf "$COL_07 MMMMMMMMIIIIIIIIIMMMMMMMMMMMMMMMMMMMM \_____||_| |_| \__  ||____/  \___/ /_/\_\ \n"
+printf "$COL_08 MMMMM77IIIIIIIIMMMMMMMMMMMMMMMMMMMMMMM               (____/              recipe \n"
+printf "$COL_09 MMMM7IIIIIIII7MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n"
+printf "$COL_10 MMZIIIIIIIII7MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM..OM\n"
+printf "$COL_11 MMIIIIIIIIIIMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN...M\n"
+printf "$COL_12 MIIIIIIIIIIIMDDDMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMOZM\n"
+printf "$COL_13 DIIIIIIIIIIIIIII7I77MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n"
+printf "$COL_14 IIIIIIIIIII.........IZMM...MMMMMMMMMMMMMMMMMMMMMMM...MMMM...MMMMMMMMMMMMMMMMMMMM\n"
+printf "$COL_15 IIIIIIIIIIIDI7III....I7M...MMMMMMMMMMMMMMMMMMMMMMM,..MMMMMMMMMMMMMMMMMMMMMMMMMMM\n"
+printf "$COL_16 DIIIIIIIIIII7. .. .. IIM...M8.. MM.. ...M ......M ......M...MM..... .MMM... ...M\n"
+printf "$COL_17 MIIIIIIIII,..  ~=,...III... . MMMM...MMM...MMM..=M,..MMMM...M ..MMM..,MM...MM...\n"
+printf "$COL_18 MOIIIIIIII ..=MMM,...III..... MMMM..MMMM........ M,..MMMM...M ..MMM...MM..NMM...\n"
+printf "$COL_19 MMMIIIIIII,..... ....III...M...MMM..MMMMO..,MMM,MMM...  M...MM ..M:.. MM..NMM...\n"
+printf "$COL_20 MMMMM7IIIIID...,I?...III...MM,.. M..MMMMMM... . MMM,. ..M ..MMM.    MMMM  NMM   \n"
+printf "$COL_21 MMMMMMMMN888MIIIIIIIIIIMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n"
+printf "$COL_22 MMMMMMMMMMMMIIIIIIIIII7MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n"
+printf "$COL_23 MMMMMMMMMMM7IIIIIIIIIIMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM powered by Akretion!\n"
+printf "$RESET" # Reset colors to "normal"
+echo ""
